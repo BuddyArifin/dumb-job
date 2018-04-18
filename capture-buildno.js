cc96afb2666e6ea14698307d6b6d9f2a34a56a56
Buddy Arifin <buddy.arifin@bizzy.co.id>
2018-04-18 14:05:46 +0700

    adding new console for print the build url

--- capture-buildno.js
+++ capture-buildno.js
@@ -1,3 +1,5 @@
 var build_number = process.env.BUILD_NO;
+var build_url = process.env.BUILD_URL;
 
 console.log('print build number', build_number);
+console.log('print build job url', build_number);
