6b9908f76f879acbed8188769e1cbc27bd8590d9
Buddy Arifin <buddy.arifin@bizzy.co.id>
2018-04-18 14:08:50 +0700

    update build url on the js file

--- capture-buildno.js
+++ capture-buildno.js
@@ -2,4 +2,4 @@ var build_number = process.env.BUILD_NO;
 var build_url = process.env.BUILD_URL;
 
 console.log('print build number', build_number);
-console.log('print build job url', build_number);
+console.log('print build job ', build_url);
