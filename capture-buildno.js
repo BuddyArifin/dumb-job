66304851797889446a5f88857769deb1150d902b
Buddy Arifin <buddy.arifin@bizzy.co.id>
2018-04-20 14:57:09 +0700

    update jenkinsfile

--- capture-buildno.js
+++ capture-buildno.js
@@ -1,2 +1,8 @@
+var build_number = process.env.BUILD_NUMBER;
+var build_url = process.env.JOB_URL;
+
+console.log('print build number', build_number);
+console.log('print build job ', build_url);
+
 console.log('Direct Access to Jenkins Variable', process.env.BUILD_NUMBER);
 console.log('Direct Access to Jenkins Variable 2', process.env.JOB_URL);
